c9142b014d6f4e16c1bb9ba145d6ff333c2043c7
Ryan Ersland <rersland@athenahealth.com>
2013-12-12 13:25:28 -0500

    end of codefu session: added a few unit tests

--- com/gildedrose/GildedRoseTest.java
+++ com/gildedrose/GildedRoseTest.java
@@ -5,12 +5,40 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+	//private GildedRose app;
+/*
+	@Before
+	public void setUp() {
+	//	this.app = new GildedRose();
+	}
+*/
 	@Test
 	public void testAgedBrie() {
-		Item[] items = new Item[] {  };
-				new Item("Aged Brie", 2, 0),
+		Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		assertEquals("fixme", app.items[0].name);
+		assertEquals(1, app.items[0].quality);
 	}
+
+	@Test	
+	public void testBackstage() {
+		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 0) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(1, app.items[0].quality);
+		for (int i = 0;i < 5;++i) {
+			app.updateQuality();
+		}
+		assertEquals(7, app.items[0].quality);
+		for (int i = 0;i < 5 ;++i) {
+			app.updateQuality();
+		}
+		assertEquals(18, app.items[0].quality);
+		for (int i = 0;i < 5;++i) {
+			app.updateQuality();
+		}
+		assertEquals(0, app.items[0].quality);
+		
+	}
+
 }
